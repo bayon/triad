4c40f10f2e635a7f7ec4f86e3d8f572016613363
Bayon Forte <bayon@Bayons-MacBook-Pro.local>
2019-01-07 13:15:21 -0500

    successfully pulled data down from nodejs api to react

--- website/triad-web/src/App.js
+++ website/triad-web/src/App.js
@@ -7,32 +7,31 @@ class App extends Component {
   constructor () {
     super()
     this.state = {
-      snipData: [],
+      todoData: [],
     }
   }
 
   componentDidMount() {
-    fetch('http://www.forteworks.com/api/simple-api.php/snips/javascript')
+    fetch('http://ec2-3-85-215-230.compute-1.amazonaws.com:3000/api/todo/',{mode: 'cors'})
     .then(results => {
       return results.json();
        
     }).then(data => {
-      console.log(data);
+      //console.log(data);
+      //console.log(typeof data);
       
-      let snipData = data.map((snip) => {
+      let todoData = data.todos.map((snip) => {
         return(
-          <li key={snip.id} className="collection-item" >
+          <li key={snip._id} className="collection-item" >
             
-            <div className="list-item-text">{snip.desc}</div>
-            <div className="list-item-snip"><span dangerouslySetInnerHTML={{__html: unescape(snip.snip)}} /></div>
+            <div className="list-item-text">{snip.item}</div>
             
             
           </li>
         )
       })
-      this.setState({snipData: snipData});
-      
-      console.log("state", this.state.snipData);
+      this.setState({todoData: todoData});
+      //console.log("state", this.state.todoData);
     })
   }
    
@@ -42,7 +41,7 @@ class App extends Component {
     return (
       <div className="App">
         
-        <List listHeader="snips" listData={this.state.snipData}></List>
+        <List listHeader="todos" listData={this.state.todoData}></List>
 
       </div>
     );
